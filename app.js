9c56d7b163fed75b494b3129fa73e76dcf172271
sendistephen <sendi.steve2@gmail.com>
2021-06-14 00:07:49 +0300

    Add validation handler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 
 require('dotenv').config();
 
@@ -20,7 +21,9 @@ mongoose
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
+app.use(expressValidator());
 
+// routes
 app.use('/api/v1/', require('./routes/user'));
 
 const port = process.env.PORT;
--- models/user.js
+++ models/user.js
@@ -13,6 +13,7 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       trim: true,
+      unique: true,
       required: true,
       maxlength: 32,
     },
