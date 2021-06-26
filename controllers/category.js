e6fdd3f49994159f152c7c7751b806a77b85d588
sendistephen <sendi.steve2@gmail.com>
2021-06-26 18:06:32 +0300

    Add read category feature

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,21 @@
 const Category = require('../models/category');
 const { errorHandler } = require('../helpers/mongoError');
 
+exports.getCategoryById = (req, res, next, categoryId) => {
+  Category.findById(categoryId).exec((err, foundCategory) => {
+    if (err || !foundCategory) {
+      return res.status(400).json({ error: 'Category not found' });
+    }
+    // add category to the req object
+    req.category = foundCategory;
+    next();
+  });
+};
+
+exports.read = (req, res) => {
+  return res.json(req.category);
+};
+
 exports.create = (req, res) => {
   const category = new Category(req.body);
   category.save((err, data) => {
--- routes/category.js
+++ routes/category.js
@@ -6,11 +6,13 @@ const {
   isAuthorized,
 } = require('../controllers/auth');
 
-const { create } = require('../controllers/category');
+const { create, read, getCategoryById } = require('../controllers/category');
 const { getUserById } = require('../controllers/user');
 
+router.get('/category/:categoryId', read);
 router.post('/category/create/:userId', isAuthenticated, isAdmin, create);
 
 router.param('userId', getUserById);
+router.param('categoryId', getCategoryById);
 
 module.exports = router;
