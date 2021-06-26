a8cbdc0f003796929077f6ec6cbdc5613888684b
sendistephen <sendi.steve2@gmail.com>
2021-06-26 15:19:52 +0300

    Add product model

---
+++ models/product.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+      maxLength: 32,
+    },
+    description: { type: String, trim: true, required: true, maxLength: 2000 },
+    price: { type: Number, trim: true, required: true, maxLength: 32 },
+    category: { type: ObjectId, ref: 'Category', required: true },
+    quantity: { type: Number },
+    photo: {
+      type: Buffer,
+      contentType: String,
+    },
+    shipping: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Product', ProductSchema);
