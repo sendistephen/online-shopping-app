960f69de9d2117665b756a515eda0fe83407d9cf
sendistephen <sendi.steve2@gmail.com>
2021-06-26 18:11:59 +0300

    Add feature delete category

--- controllers/category.js
+++ controllers/category.js
@@ -16,6 +16,20 @@ exports.read = (req, res) => {
   return res.json(req.category);
 };
 
+exports.deleteCategory = (req, res) => {
+  const category = req.category;
+
+  category.remove((err, deletedCategory) => {
+    if (err) {
+      return res.status({ error: errorHandler });
+    }
+    return res.json({
+      message: 'Category deleted successfully!',
+      deletedCategory,
+    });
+  });
+};
+
 exports.create = (req, res) => {
   const category = new Category(req.body);
   category.save((err, data) => {
--- routes/category.js
+++ routes/category.js
@@ -6,10 +6,16 @@ const {
   isAuthorized,
 } = require('../controllers/auth');
 
-const { create, read, getCategoryById } = require('../controllers/category');
+const {
+  create,
+  read,
+  getCategoryById,
+  deleteCategory,
+} = require('../controllers/category');
 const { getUserById } = require('../controllers/user');
 
 router.get('/category/:categoryId', read);
+router.delete('/category/:categoryId/:userId', deleteCategory);
 router.post('/category/create/:userId', isAuthenticated, isAdmin, create);
 
 router.param('userId', getUserById);
