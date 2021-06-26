d7e2e68139adeb39c346fcebf64027ddff7cc2c3
sendistephen <sendi.steve2@gmail.com>
2021-06-26 16:49:23 +0300

    Add form data validation on product creation

--- controllers/product.js
+++ controllers/product.js
@@ -13,9 +13,20 @@ exports.create = (req, res) => {
     if (err) {
       return res.status(400).json({ error: 'Image could not be uploaded' });
     }
+    // check for all form fields
+    const { name, description, price, category, quantity, shipping } = fields;
+    if ((!name || !description || !price, !category, !quantity, !shipping)) {
+      return res.json({ error: 'All fields are required!' });
+    }
     // create new product
     let product = new Product(fields);
     if (files.photo) {
+      // limit photo size
+      if (files.photo.size > 1000000) {
+        return res.json({
+          error: 'Image size should be less than 1mb in size',
+        });
+      }
       // access file system
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
