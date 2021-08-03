99253b212ee0559c10811f8e88931376332192ba
sendistephen <sendi.steve2@gmail.com>
2021-08-04 00:22:11 +0300

    Add product details

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -1,29 +1,46 @@
 import ProductImage from 'components/ProductImage';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const Product = ({ product, showViewProductButton = true }) => {
   const showViewButton = (showViewProductButton) => {
     return (
       showViewProductButton && (
         <Link to={`/products/${product._id}`}>
-          <button className='btn btn-success  mt-2 mb-2'>
-            View Details
-          </button>
+          <button className='btn btn-success  mt-2 mb-2'>View Details</button>
         </Link>
       )
     );
   };
+  const showStock = (quantity) => {
+    return quantity > 0 ? (
+      <span className='badge badge-success badge-pill'>In Stock</span>
+    ) : (
+      <span className='badge badge-danger badge-pill'>Out of Stock</span>
+    );
+  };
   return (
     <div className='card'>
-      <div className='card-header'>{product.name}</div>
+      <div className='card-header name'>{product.name}</div>
       <div className='card-body'>
         <ProductImage item={product} url='product' />
-        <p className='card-text'>{product.description.substring(0, 50)}</p>
-        <p className='card-text'>$ {product.price}</p>
+        <p className='card-text lead'>{product.description.substring(0, 50)}</p>
+        <p className='card-text black-10'>$ {product.price}</p>
+        <div className='black-9'>
+          <span className='mr-2'>Category</span>
+          <span className='badge bg-light text-dark'>
+            {product.category && product.category.name}
+          </span>
+        </div>
         {showViewButton(showViewProductButton)}
-
-        <button className='btn btn-dark  mt-2 mb-2 ml-2'>Add to Cart</button>
+        <p className='black-8 my-2'>
+          Added {moment(product.createdAt).fromNow()}
+        </p>
+        <div>{showStock(product.quantity)}</div>
+        <button className='btn btn-sm btn-dark  mt-2 mb-2 mr-2'>
+          Add to Cart
+        </button>
       </div>
     </div>
   );
