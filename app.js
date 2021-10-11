84755a0f7d006ff33467fd30fdb51a193cf56aab
sendistephen <sendi.steve2@gmail.com>
2021-10-11 16:26:26 +0300

    Configure deployment by vercel

--- app.js
+++ app.js
@@ -35,9 +35,9 @@ app.use('/api/v1/', require('./routes/braintree'));
 app.use('/api/v1/', require('./routes/order'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, './client/build/index.html'))
   );
 }
 
